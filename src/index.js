e8861f123b48a7eb3086d481efeab9beec167b6f
Bahoz <bahozali50@gmail.com>
2022-07-07 00:53:31 +0300

    refactor displayTasks to displayAllTasks and Create single task function, adding functionality is working now

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
-import { createLocalStorage } from './services';
+import { createLocalStorage, addData, getData } from './services';
 import './style.css';
 
 const list = document.querySelector('.todo_list');
+const taskInput = document.querySelector('#task_input');
+const addTaskButton = document.querySelector('#add_task');
+const taskForm = document.querySelector('#task_form');
 
 export let tasks = [
   { description: 'Buy new phone', completed: false, index: 1 },
@@ -9,22 +12,42 @@ export let tasks = [
   { description: 'write a new book', completed: false, index: 3 },
 ];
 
-function displayTodo() {
-  tasks.forEach(({ completed, description }) => {
-    const li = document.createElement('li');
-    li.classList.add('item');
+function createTaskElement(description, completed = false) {
+  const li = document.createElement('li');
+  li.classList.add('item');
 
-    li.innerHTML = `<div class="group"><input type="checkbox" ${
-      completed && 'checked'
-    }/>
+  li.innerHTML = `<div class="group"><input type="checkbox" ${
+    completed && 'checked'
+  }/>
                      <p>${description}</p> </div> 
                      <i class="fa-solid fa-ellipsis-vertical"></i>`;
+  return li;
+}
 
+function displayAllTasks(tasks) {
+  tasks.forEach(({ completed, description }) => {
+    const li = createTaskElement(description, completed);
     list.appendChild(li);
   });
 }
 
+taskForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  if (taskInput.value.trim() === '') return;
+
+  const index = getData('tasks').length + 1;
+  const task = { description: taskInput.value, index, completed: false };
+
+  addData(task);
+
+  const li = createTaskElement(task.description);
+  list.appendChild(li);
+
+  taskInput.value = '';
+});
+
 document.addEventListener('DOMContentLoaded', () => {
   createLocalStorage('tasks', []);
-  displayTodo();
+  displayAllTasks(getData('tasks'));
 });
--- src/services.js
+++ src/services.js
@@ -1,6 +1,6 @@
 export function saveTOLocalStorage() {}
 
-export function createLocalStorage(key = 'tasks', value=[]) {
+export function createLocalStorage(key = 'tasks', value = []) {
   if (!getData(key)) {
     localStorage.setItem(key, JSON.stringify(value));
   }
@@ -8,10 +8,19 @@ export function createLocalStorage(key = 'tasks', value=[]) {
 
 export function getData(key) {
   try {
-    return JSON.stringify(localStorage.getItem(key));
+    return JSON.parse(localStorage.getItem(key));
   } catch (error) {
     return localStorage.getItem(key);
   }
+}
+
+export function addData(obj) {
+  console.log({obj})
+  const tasks = getData('tasks');
+  tasks.push(obj)
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+
+  return tasks;
 }
 
 export function setData() {}
