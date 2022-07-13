98434a7f086304ddbf6d839919600895cd92c635
Abdullah2213565 <dulakhan024@gmail.com>
2022-07-13 17:02:58 +0200

    Trying to fix the testing error

--- tests/add_delete.test.js
+++ tests/add_delete.test.js
@@ -1,14 +1,31 @@
-// import { addItem } from '../src/dom';
+/**
+ * @jest-environment jsdom
+ */
+
+
+import {addData, getData} from '../src/services.js'
+
+
 
 describe('add and delete testing', () => {
   it('should be test', () => {
-
-
-    const name = "test"
-
-    expect(name).toBe('test')
+    const task1 = {
+      description: 'Head back home',
+      completed: false,
+      index: 1,
+    };
+    const task2 = {
+      description: 'clean',
+      completed: false,
+      index: 2,
+    };
+    addData(task1);
+    expect(getData('tasks')).toHaveLength(1);
+    expect(getData('tasks')[0].completed).toBe(false);
+    addData(task2);
+    expect(getData('tasks')).toHaveLength(2);
+    expect(getData('tasks')[1].description).toBe('clean');
   });
-
 
 
 });
